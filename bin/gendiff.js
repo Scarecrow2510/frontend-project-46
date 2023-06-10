4c8d29771c9848b6094ab6bbb3dcc1245fb9b0be
Denis <mr.denis.shchukin@mail.ru>
2023-06-10 13:53:57 +0300

    Fixes commit 8

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -1,18 +1,15 @@
 #!/usr/bin/env node
-import { program } from 'commander';
+import { Command } from 'commander/esm.mjs';
 import genDiff from '../src/index.js';
 
+const program = new Command();
+
 program
-  .name('gendiff')
-  .version('3.4.4', '-V, --vers', 'output the current version')
   .description('Compares two configuration files and shows a difference.')
-  .helpOption('-h, --help', 'output help message')
-  .option('-f, --format [type]', 'output format', 'stylish')
-  .argument('<filetype1>')
-  .argument('<filetype2>')
-  .action((filetype1, filetype2) => {
-    const formatType = program.opts().format;
-    const result = genDiff(filetype1, filetype2, formatType);
-    console.log(result);
-  });
-program.parse();
+  .version('3.4.4')
+  .option('-f, --format <type>', 'output format', 'stylish')
+  .argument('<filepath1>')
+  .argument('<filepath2>')
+  .action((filepath1, filepath2) => console
+    .log(genDiff(filepath1, filepath2, program.opts().format)))
+  .parse(process.argv);
