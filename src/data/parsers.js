003ac7fe3c07794f5a3936eb2aea77bef9c0f93d
Denis <mr.denis.shchukin@mail.ru>
2023-06-10 13:26:07 +0300

    Fixes commit 5

--- src/data/parsers.js
+++ src/data/parsers.js
@@ -4,11 +4,11 @@ import YAML from 'js-yaml';
 
 export default (fileData, fileExt) => {
   switch (fileExt) {
-    case 'json':
+    case '.json':
       return JSON.parse(fileData);
-    case 'yml':
+    case '.yml':
       return YAML.load(fileData);
-    case 'yaml':
+    case '.yaml':
       return YAML.load(fileData);
     default:
       throw new Error(`Invalid format: ${fileExt}`);
